b9befa33a13ee21523fee05902927fb5099d8bdf
enBIJOY <mofizuldev@gmail.com>
2026-02-07 13:59:22 +0600

    sidebar not active page status

--- resources/views/frontend/layout/sidebar.blade.php
+++ resources/views/frontend/layout/sidebar.blade.php
@@ -4,14 +4,13 @@
         <h1 id="colorlib-logo"><a href="{{url('/')}}">elen<span>.</span></a></h1>
         <nav id="colorlib-main-menu" role="navigation">
             <ul>
-                <li class="colorlib-active"><a href="{{ Route('blog') }}">BLOG's</a></li>
-                <li><a href="{{ Route('about') }}">About Us</a></li>
-                <li><a href="{{ Route('contact') }}">Contact Us</a></li>
-                <li><a href="{{ Route('skills') }}">Skills And Activity</a></li>
-                <li><a href="{{ Route('privecyPolicy') }}">Privecy & Policy</a></li>
+                <li class="{{ request()->routeIs('blog') ? 'active' : '' }}"><a href="{{ route('blog') }}">BLOG's</a></li>
+                <li class="{{ request()->routeIs('about') ? 'active' : '' }}"><a href="{{ route('about') }}">About Us</a></li>
+                <li class="{{ request()->routeIs('contact') ? 'active' : '' }}"><a href="{{ route('contact') }}">Contact Us</a></li>
+                <li class="{{ request()->routeIs('skills') ? 'active' : '' }}"><a href="{{ route('skills') }}">Skills And Activity</a></li>
+                <li class="{{ request()->routeIs('privecyPolicy') ? 'active' : '' }}"><a href="{{ route('privecyPolicy') }}">Privacy & Policy</a></li>
             </ul>
         </nav>
-
         <div class="colorlib-footer">
             <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
