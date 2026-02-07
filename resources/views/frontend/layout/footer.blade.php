0b12566b69ab6e4226cabc726a98740337834462
enBIJOY <mofizuldev@gmail.com>
2026-02-07 13:00:01 +0600

    Working on footer

--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -55,13 +55,13 @@
     <div class="row">
       <!--Grid column-->
       <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Footer text</h5>
+        <h5 class="text-uppercase text-white">Social Handels</h5>
 
-        <p>
+        <p class="px-3">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-          voluptatem veniam, est atque cumque eum delectus sint!
+          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae!
         </p>
+        <div class="p-3"></div>
       </div>
       <!--Grid column-->
 
@@ -74,6 +74,7 @@
           molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
           voluptatem veniam, est atque cumque eum delectus sint!
         </p>
+        
       </div>
       <!--Grid column-->
     </div>
