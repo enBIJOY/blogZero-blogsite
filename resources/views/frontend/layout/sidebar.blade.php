5ad4208e110d791a43351ef9e65a0913fe8d48a2
enBIJOY <mofizuldev@gmail.com>
2026-02-08 00:54:45 +0600

    frontend works

--- resources/views/frontend/layout/sidebar.blade.php
+++ resources/views/frontend/layout/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div id="colorlib-page">
 	<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
     <aside id="colorlib-aside" role="complementary" class="js-fullheight text-center">
-        <h1 id="colorlib-logo"><a href="{{url('/')}}">elen<span>.</span></a></h1>
+        <h1 id="colorlib-logo"><span>.</span><a href="{{url('/')}}">Chen<span>.</span></a></h1>
         <nav id="colorlib-main-menu" role="navigation">
             <ul>
                 <li class="{{ request()->routeIs('blog') ? 'active' : '' }}"><a href="{{ route('blog') }}">BLOG's</a></li>
@@ -12,13 +12,18 @@
             </ul>
         </nav>
         <div class="colorlib-footer">
+            
+            <div class="mb-3 ">
+                <button class="button button-outline-success btn-sm" href="">Login As Admin</button>
+            </div>
+
             <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://github.com/enbijoy/blogZero-blogsite" target="_blank">EnBijoy</a>
             <ul>
-                <li><a href="https://facebook.com" target="_blank"><i class="icon-facebook"></i></a></li>
+                <li><a href="https://facebook.com/mohammadbijoy.dm" target="_blank"><i class="icon-facebook"></i></a></li>
                 <li><a href="https://x.com" target="_blank"><i class="icon-twitter"></i></a></li>
                 <li><a href="https://instagram.com" target="_blank"><i class="icon-instagram"></i></a></li>
-                <li><a href="https://linkedin.com/in" target="_blank"><i class="icon-linkedin"></i></a></li>
+                <li><a href="https://linkedin.com/in/mofizuldev" target="_blank"><i class="icon-linkedin"></i></a></li>
             </ul>
         </div>
     </aside> <!-- END COLORLIB-ASIDE -->
